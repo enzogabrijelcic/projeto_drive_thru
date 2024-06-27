ceb180f6d4f572b391c38c09c692f474ab800ed0
Feeeeeh <klitzketech@gmail.com>
2024-06-27 14:27:51 -0300

    atualizando pagamento dnv

--- pagamento.py
+++ pagamento.py
@@ -1,11 +1,14 @@
 #formas de pagamento: cartão, pix, dinheiro
 import time
+from formatação import *
 def forma_pagamento(n_pedido):
-    forma_pag = input('Qual a forma de pagamento?\n1. Cartão \n2. Dinheiro \n3. pix\n4. Cancelar pedido\n opção:')
+    forma_pag = typing('Qual a forma de pagamento?')
+    print('1. Cartão \n2. Dinheiro \n3. pix\n4. Cancelar pedido')
+    forma_pag = input()
     if forma_pag == '1':
         print("Cartão Selecionado.") 
         time.sleep(1)
-        print("Processando pagamento",time.sleep(0.5),".",time.sleep(0.5),".")
+        print("Processando pagamento",end = ""),slow_typing("...")
         print('nº do pedido: ', n_pedido)
     elif forma_pag == '2':
         print("Dinheiro Selecionado")
@@ -17,7 +20,7 @@ def forma_pagamento(n_pedido):
         time.sleep(1)
         print('chave Pix:2a1sd6a51sd6a1sd6a51s6da1s')
         time.sleep(3)
-        print("Processando pagamento",time.sleep(0.5),".",time.sleep(0.5),".")
+        print("Processando pagamento",end = ""),slow_typing("...")
         print('seu pedido é nº:', n_pedido)
     elif forma_pag == '4':
         print("Pedido cancelado.")
